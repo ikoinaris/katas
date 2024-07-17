109bcf5887a236158731b94ab47622a1b18c8148
ikoinaris <i.koinaris@gmail.com>
2024-07-17 10:35:41 +0300

    Leaderboard updated

--- Racing Car/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
+++ Racing Car/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
@@ -1,7 +1,6 @@
 package tddmicroexercises.leaderboard;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import tddmicroexercises.leaderboard.models.Race;
 import tddmicroexercises.leaderboard.services.RankingsCalculatorImpl;
 import tddmicroexercises.leaderboard.services.ResultsCalculatorImpl;
@@ -16,11 +15,6 @@ public class Leaderboard {
     private ResultsCalculatorImpl resultsCalculator;
     private List<Race> races;
 
-    private Map<String, Integer> results;
-    public Leaderboard() {
-        results = driverResults();
-    }
-
     public Leaderboard(Race... races) {
         this.races = Arrays.asList(races);
     }
@@ -30,7 +24,7 @@ public class Leaderboard {
     }
 
     public List<String> driverRankings() {
-        return rankingsCalculator.calculateRankings(results);
+        return rankingsCalculator.calculateRankings(driverResults());
     }
 
 }
