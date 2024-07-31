4ce8ff4ee8600a52e06fb369b0d3154e7c6bf402
ikoinaris <i.koinaris@gmail.com>
2024-07-31 11:19:44 +0300

    MarsRoverTest obstacle and edge reached test cases implemented

--- Mars Rover Kata/MarsRover/src/test/java/model/MarsRoverTest.java
+++ Mars Rover Kata/MarsRover/src/test/java/model/MarsRoverTest.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 
@@ -11,18 +10,51 @@ class MarsRoverTest {
     @InjectMocks
     private MarsRover marsRover;
 
-    @BeforeEach
-    void setUp() {
-        marsRover = new MarsRover(1,2, 'N');
-    }
-
     @Test
-    void moveCommands() {
+    void givenCharArrayOfCommands_whenMoveCommandsCalled_thenPositionAndDirectionUpdated() {
+
+        //Given
+        marsRover = new MarsRover(1,2, 'N', new int[][]{}, 20, 20);
         var commands = new char[]{'F', 'L', 'F', 'R', 'F', 'L', 'B'};
+
+        // When
         marsRover.moveCommands(commands);
 
+        // Then
         assertEquals(marsRover.getDirection(), 'W');
         assertEquals(marsRover.getPositionX(), 1);
         assertEquals(marsRover.getPositionY(), 4);
+    }
+
+    @Test
+    void givenCharArrayOfCommands_WhenObstacleDetected_thenMoveToPreviousPosition() {
+
+        //Given
+        marsRover = new MarsRover(5,5, 'N', new int[][]{{5, 6}}, 20, 20);
+        var commands = new char[]{'F', 'L', 'F', 'R', 'F', 'L', 'B'};
+
+        // When
+        marsRover.moveCommands(commands);
+
+        // Then
+        assertEquals(0, marsRover.getPositionX());
+        assertEquals(0, marsRover.getPositionY());
+
+    }
+
+    @Test
+    void givenCharArrayOfCommands_whenAreasEdgesReached_thenDirectionChanged() {
+
+        //Given
+        marsRover = new MarsRover(1,4, 'N', new int[][]{}, 5, 5);
+        var commands = new char[]{'F', 'L', 'F', 'R', 'F', 'L', 'B'};
+
+        // When
+        marsRover.moveCommands(commands);
+
+        // Then
+        assertEquals(marsRover.getDirection(), 'W');
+        assertEquals(1, marsRover.getPositionX());
+        assertEquals(0, marsRover.getPositionY());
     }
 }
